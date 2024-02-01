44859fc496de99603315d1ddf91fe20c88eab334
Emilion <emiljadersten@gmail.com>
2024-02-01 09:54:26 +0100

    Update allowed hosts

--- fooroom/settings.py
+++ fooroom/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#y78eyc+d^c6=ndwcd=66tk^&b)87*am%7ym+c%j3iej%vnbce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+]
 
 
 # Application definition
