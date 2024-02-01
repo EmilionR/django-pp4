0a816cc4d44a423c13b1ddbec9ce3d92e21db637
Emilion <emiljadersten@gmail.com>
2024-02-01 14:02:43 +0100

    Create comment model and add comments

--- board/models.py
+++ board/models.py
@@ -4,14 +4,29 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Post(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+
+class Entry(models.Model):
     body = models.TextField()
-    preview = models.TextField(blank=True)
     posted_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     likes = models.IntegerField(default=0)
+    class Meta:
+        abstract = True
+
+
+class Post(Entry):
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="board_posts"
+    )
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    preview = models.TextField(blank=True)
+
+
+class Comment(Entry):
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="board_replies"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
