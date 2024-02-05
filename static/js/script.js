32f54b1a86d778b2952ebcdb627b4640937edb2d
Emilion <emiljadersten@gmail.com>
2024-02-05 18:02:34 +0100

    Enable opening and closing the comment form

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+
+
+function openForm() {
+    document.getElementById("posting-form").style.display = "block";
+    document.getElementById("posting-button").style.display = "none";
+}
+
+function closeForm() {
+    document.getElementById("posting-form").style.display = "none";
+    document.getElementById("posting-button").style.display = "block";
+}
