7891a51cdb1f9da5bb20b22a7d33b1e3e6f580c0
Emilion <emiljadersten@gmail.com>
2024-02-05 09:28:21 +0100

    Add post detail page

--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post, Comment
 
@@ -7,3 +7,17 @@ from .models import Post, Comment
 class PostList(generic.ListView):
     queryset = Post.objects.all().order_by("-is_sticky", "-posted_on")
     template_name = "board/index.html"
+
+def post_detail(request, slug):
+    """
+    Display an individual post thread
+    """
+
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "board/post_detail.html",
+        {"post": post},
+    )
