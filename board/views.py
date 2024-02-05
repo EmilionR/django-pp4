a2e0347a2dcd94b0e02c32c287a93836dfbb4fa4
Emilion <emiljadersten@gmail.com>
2024-02-05 16:31:49 +0100

    Add comment form

--- board/views.py
+++ board/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post, Comment
+from .models import Post
+from .forms import CommentForm
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -16,9 +19,22 @@ def post_detail(request, slug):
     queryset = Post.objects.all()
     post = get_object_or_404(queryset, slug=slug)
 
-    comments = post.comments.all().order_by("posted_on")
+    comments = post.comments.all().order_by("-is_sticky", "posted_on")
     comment_count = post.comments.all().count()
 
+    comment_form = CommentForm()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment posted successfully'
+            )
 
     return render(
         request,
@@ -27,5 +43,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
          },
     )
