c4b0506cf8a7a1f6b5a0e6a05cec57d56fe670db
Emilion <emiljadersten@gmail.com>
2024-02-05 12:11:30 +0100

    Configure user profiles (model, template, admin) and add profile to existing users

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -6,12 +7,18 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.CharField(max_length=500, default='No bio...')
     posts = models.ManyToManyField('board.Post', related_name='user_posts')
-    posts = models.ManyToManyField('board.Comment', related_name='user_comments')
+    comments = models.ManyToManyField('board.Comment', related_name='user_comments')
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
+
+class ProfileAdmin(admin.ModelAdmin):
+    exclude = ('posts', 'comments')
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        return form
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 
