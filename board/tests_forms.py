8f77e3a195de6c361da164aebff82aaa4a7fe28b
Emilion <emiljadersten@gmail.com>
2024-02-24 15:08:54 +0100

    Create tests for board forms

---
+++ board/tests_forms.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from .forms import PostForm, CommentForm, EditPostForm
+
+# Create your tests here.
+
+class TestPostForm(TestCase):
+
+    def test_form_is_valid(self):
+        post_form = PostForm({'title': 'Testing.', 'body': 'This is a test.'})
+        self.assertTrue(post_form.is_valid())
+    
+    def test_form_is_invalid(self):
+        post_form = PostForm({'title': '', 'body': ''})
+        self.assertFalse(post_form.is_valid(), msg="Form is valid")
+
+
+class TestCommentForm(TestCase):
+
+    def test_form_is_valid(self):
+        comment_form = CommentForm({'body': 'This is a test.'})
+        self.assertTrue(comment_form.is_valid())
+    
+    def test_form_is_invalid(self):
+        comment_form = CommentForm({'body': ''})
+        self.assertFalse(comment_form.is_valid(), msg="Form is valid")
+
+
+class TestEditForm(TestCase):
+
+    def test_form_is_valid(self):
+        edit_form = EditPostForm({'body': 'This is a test.'})
+        self.assertTrue(edit_form.is_valid())
+    
+    def test_form_is_invalid(self):
+        edit_form = EditPostForm({'body': ''})
+        self.assertFalse(edit_form.is_valid(), msg="Form is valid")
--- fooroom/settings.py
+++ fooroom/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 
 if os.path.isfile("env.py"):
@@ -95,6 +96,9 @@ WSGI_APPLICATION = "fooroom.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = ["https://127.0.0.1", "https://*.herokuapp.com"]
 
