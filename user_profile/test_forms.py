52d06832ea6197c5ada4b72642fe78348bf64d3c
Emilion <emiljadersten@gmail.com>
2024-02-25 15:49:23 +0100

    Create tests for profile form

--- user_profile/test_forms.py
+++ user_profile/test_forms.py
@@ -1,3 +1,31 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from .forms import ProfileForm
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileFormTest(TestCase):
+    def test_clean_avatar(self):
+        # Create a file larger than the size limit
+        large_file = SimpleUploadedFile("large_image.jpg", b" " * (524288 + 1))
+
+        # Create form data with the large file
+        form_data = {'avatar': large_file}
+        form = ProfileForm(data={'about': 'test bio'}, files=form_data)
+
+        # Check if the form is rejected due to file size
+        self.assertFalse(form.is_valid())
+        self.assertTrue('avatar' in form.errors)
+        self.assertEqual(form.errors['avatar'], ['File size must be less than 0.5 MB.'])
+
+    def test_clean_avatar_valid(self):
+
+        # Create a file smaller than the size limit
+        small_file = SimpleUploadedFile("small_image.jpg", b" " * (524288 - 1))
+
+        # Create form data with the small file
+        form_data = {'avatar': small_file}
+        form = ProfileForm(data={'about': 'test bio'}, files=form_data)
+
+        # Check if the form is valid
+        self.assertTrue(form.is_valid())
