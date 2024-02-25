569a13c5779dbd68e124c289cc30e92a706b7a01
Emilion <emiljadersten@gmail.com>
2024-02-25 15:33:03 +0100

    Create views tests for profile

--- user_profile/test_views.py
+++ user_profile/test_views.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test_user",
+            password="qwertyuiopå"
+        )
+
+    def test_get_profile_page(self):
+        response = self.client.get(f"/profile/{self.user.username}")
+        self.assertEqual(response.status_code, 301)
